df52c7a17a03550598fbbead66ef6a1134585c2b
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-08-31 18:41:42 +0800

    ✨ feat: add navigation menu for blog navigation

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,38 @@
 </script>
 
 <template>
+    <header>
+        <!-- Navigation -->
+        <div>
+            <div></div>
+            <p>Hello, HENNGE!</p>
+            <button>+</button>
+        </div>
+
+        <!-- Menu -->
+         <div>
+            <ul>
+                <li>
+                    <a href="#introduction">Who I am</a>    
+                </li>
+                <li>
+                    <a href="#drive-for-japan">The Drive for Japan</a>
+                </li>
+                <li>
+                    <a href="#training-arc">The Training Arc</a>
+                </li>
+                <li>
+                    <a href="#the-goal">Introduction to HENNGE</a>
+                </li>
+                <li>
+                    <a href="#the-journey">HENNGE Journey</a>
+                </li>
+                <li>
+                    <a href="#afterwords">Afterwords</a>
+                </li>
+            </ul>
+         </div>
+    </header>
 </template>
 
 <style scoped>
