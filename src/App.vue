197a6dcf5fd60b6e7391a31c2ed525ed0c969750
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-08-31 18:45:06 +0800

    ✨ feat: add main content sections for content to go in

--- src/App.vue
+++ src/App.vue
@@ -34,6 +34,27 @@
             </ul>
          </div>
     </header>
+
+    <main>
+        <section id="introduction">
+            <h2>Who I am</h2>
+        </section>
+        <section id="drive-for-japan">
+            <h2>The Drive for Japan</h2>
+        </section>
+        <section id="training-arc">
+            <h2>The Training Arc</h2>
+        </section>
+        <section id="the-goal">
+            <h2>Introduction to HENNGE</h2>
+        </section>
+        <section id="the-journey">
+            <h2>HENNGE Journey</h2>
+        </section>
+        <section id="afterwords">
+            <h2>Afterwords</h2>
+        </section>
+    </main>
 </template>
 
 <style scoped>
