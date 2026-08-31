284a05ccd174323648a6e17af987bf3669e9d72f
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-08-31 19:26:26 +0800

    ✨ content: add introductory paragraphs to give context about me

--- src/App.vue
+++ src/App.vue
@@ -38,6 +38,12 @@
     <main>
         <section id="introduction">
             <h2>Who I am</h2>
+            <p>
+                Hi, there! Thanks for checking this blog out. I hope this will help you to get to know me better, and have some laughs 😆. Do note that this will be a casual blog, as I believe that I can express myself better that way without thinking about formal words, just focusing on storytelling.
+            </p>
+            <p>
+                A little about myself: I am a graduating student of Information Technology from a public university in Nueva Ecija, Philippines called Nueva Ecija University of Science and Technology. Quite a long name, ain't it? Before I met HENNGE, I was already in love with Japan. The culture, the environment, the opportunities. It was just perfect!
+            </p>
         </section>
         <section id="drive-for-japan">
             <h2>The Drive for Japan</h2>
