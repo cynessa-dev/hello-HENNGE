1db7d757dc3f7831275e9a03df91dd6fb8618e25
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-08-31 21:42:48 +0800

    ✨ content: add details about getting to know HENNGE

--- src/App.vue
+++ src/App.vue
@@ -74,6 +74,15 @@
         </section>
         <section id="the-goal">
             <h2>Introduction to HENNGE</h2>
+            <p>
+                This is where it got interesting. I wanted to be structured. Have a guide to follow. So I thought, since I want to work in Japan, I should check industry standards in there. From there I listed a few Japanese companies that I found interesting. And out of those companies, HENNGE was one of best match for me. They are curious, learn-aholic, and continuously growing.
+            </p>
+            <p>
+                HENNGE's philosophy, "Liberation of Technolgy", is such an interesting mindset! I watched a video of HENNGE's CEO, Mr. Ogura, explaining that there will always be a gap between technologies and customers because technology keeps evolving, and that they wanted to keep filling the gap. After that, I just knew I gotta join them.
+            </p>
+            <p>
+                With having the HENNGE path, I wanted to adapt their philosophy. Just like how my seniors helped me to grow. I wanted to do the same with everyone. To build solutions to keep filling the gap!
+            </p>
         </section>
         <section id="the-journey">
             <h2>HENNGE Journey</h2>
