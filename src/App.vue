9993cccc1d2e7ba9334c93a7360e8c985734d6c0
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-08-31 22:07:45 +0800

    ✨ content: add closing thoughts and appreciation in the afterwords section

--- src/App.vue
+++ src/App.vue
@@ -107,6 +107,15 @@
         </section>
         <section id="afterwords">
             <h2>Afterwords</h2>
+            <p>
+                Hello! If you made it this far, thank you so much for reading my blog. I really hoped you enjoyed it. My journey towards HENNGE was life-changing. It pushed me to learn new technologies, to try out different things. I made this blog specifically for HENNGE. I wanted to thank them somehow, and show my appreciation for helping me.
+            </p>
+            <p>
+                That's why, whatever happens, I will keep on pushing towards HENNGE. If I were to say it in code: Every iteration, I increment myself. Or something like that.
+            </p>
+            <p>
+                Once again! Thank you so much for taking the time to read this. Farewell, and I hope you have a great day!
+            </p>
         </section>
     </main>
 </template>
