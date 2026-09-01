50e15cdf3390c889be173c4342c5011528aee29a
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-09-01 20:04:15 +0800

    ✏️ content(Afterwords): refine grammar to be clear

--- src/components/sections/Afterwords.vue
+++ src/components/sections/Afterwords.vue
@@ -22,8 +22,8 @@
     <p>
       Hello! If you made it this far, thank you so much for reading my blog. I really hoped you
       enjoyed it. My journey towards HENNGE was life-changing. It pushed me to learn new
-      technologies, to try out different things. I made this blog specifically for HENNGE. I wanted
-      to thank them somehow, and show my appreciation for helping me.
+      technologies, and to try out different things. I made this blog specifically for HENNGE
+      because I wanted to thank them somehow, and show my appreciation for helping me.
     </p>
     <p>
       That's why, whatever happens, I will keep on pushing towards HENNGE. If I were to say it in
