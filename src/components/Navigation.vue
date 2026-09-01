828d16bfb72823d9e4c1162ef2c892f520ebb7d2
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-09-01 14:39:34 +0800

    🎨 refactor: modularize navigation into a component for separation

---
+++ src/components/Navigation.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>
+        <div></div>
+        <p>Hello, HENNGE!</p>
+        <button>+</button>
+    </div>
+    <div>
+        <ul>
+            <li>
+                <a href="#introduction">Who I am</a>    
+            </li>
+            <li>
+                <a href="#drive-for-japan">The Drive for Japan</a>
+            </li>
+            <li>
+                <a href="#training-arc">The Training Arc</a>
+            </li>
+            <li>
+                <a href="#the-goal">Introduction to HENNGE</a>
+            </li>
+            <li>
+                <a href="#the-journey">HENNGE Journey</a>
+            </li>
+            <li>
+                <a href="#afterwords">Afterwords</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+</style>
