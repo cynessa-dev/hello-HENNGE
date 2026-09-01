0deac8cd7ad4589b578bfbaa927a0965f847becd
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-09-01 20:28:27 +0800

    ✨ feat(Header): add margin and padding to header for improved layout

--- src/components/ui/Header.vue
+++ src/components/ui/Header.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <header>
+  <header class="mt-12 mb-8">
     <h1 class="text-4xl font-bold">The Loop: A Journey of Continuous Learning</h1>
     <div class="flex space-x-2 h-10 text-muted text-sm">
       <p>Christian Mamplata</p>
