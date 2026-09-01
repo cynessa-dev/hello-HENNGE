6fe1cdc0f0857b72cd116f1ffbf7aa3bc10d1464
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-09-01 20:25:43 +0800

    ✨ feat(Header): update header title and add metadata for improved context

--- src/components/ui/Header.vue
+++ src/components/ui/Header.vue
@@ -2,7 +2,14 @@
 
 <template>
   <header>
-    <h1 class="text-xl">The Initiative: My Mindset Built a Path For Me</h1>
+    <h1 class="text-4xl font-bold">The Loop: A Journey of Continuous Learning</h1>
+    <div class="flex space-x-2 h-10 text-muted text-sm">
+      <p>Christian Mamplata</p>
+      <p>·</p>
+      <p>10 min read</p>
+      <p>·</p>
+      <p>August 31, 2026</p>
+    </div>
   </header>
 </template>
 
