fdc59fae29a790db55ba4c26dc7d5979c74349e6
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-09-01 20:09:52 +0800

    ✨ feat(VideoContainer): add VideoContainer component for video display

---
+++ src/components/ui/shared/VideoContainer.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  interface Video {
+    src: string
+    caption?: string
+  }
+
+  defineProps<{
+    video: Video
+  }>()
+</script>
+
+<template>
+  <div class="flex flex-col items-center space-y-2">
+    <div class="flex flex-wrap gap-4 w-full">
+      <video :src="video.src" class="flex-1 w-full rounded-lg" autoplay muted loop />
+    </div>
+    <p class="text-muted text-sm">{{ video.caption }}</p>
+  </div>
+</template>
+
+<style scoped></style>
