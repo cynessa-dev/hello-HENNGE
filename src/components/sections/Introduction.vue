ff26f1d63cdf95400bc986ca2ffb6c1d687abac5
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-09-01 10:12:23 +0800

    🍱 content: add image with caption in introduction

--- src/components/sections/Introduction.vue
+++ src/components/sections/Introduction.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+    import pictureOfMe from '@/assets/images/intro_0.jpg'
 </script>
 
 <template>
     <section id="introduction">
+        <img :src="pictureOfMe" alt="Picture of me" />
+        <p>Me Presenting a Prototype</p>
         <h2>Who I am</h2>
         <p>
             Hi, there! Thanks for checking this blog out. I hope this will help you to get to know me better, and have some laughs 😆. Do note that this will be a casual blog, as I believe that I can express myself better this way without thinking about formal words, just focusing on storytelling.
