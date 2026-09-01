fe742638372e14a367e98e6804323cf948cf0018
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-09-01 15:53:55 +0800

    💄 style(navigation): add initial design for navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,35 +1,31 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-    <div>
-        <div></div>
-        <p>Hello, HENNGE!</p>
-        <button>+</button>
+  <nav>
+    <div class="flex justify-between items-center px-4 py-6">
+      <h1 class="font-bold">Hello, HENNGE!</h1>
+      <ul class="flex space-x-8">
+        <li>
+          <a href="#introduction">Who I am</a>
+        </li>
+        <li>
+          <a href="#drive-for-japan">The Drive for Japan</a>
+        </li>
+        <li>
+          <a href="#training-arc">The Training Arc</a>
+        </li>
+        <li>
+          <a href="#the-goal">Introduction to HENNGE</a>
+        </li>
+        <li>
+          <a href="#the-journey">HENNGE Journey</a>
+        </li>
+        <li>
+          <a href="#afterwords">Afterwords</a>
+        </li>
+      </ul>
     </div>
-    <div>
-        <ul>
-            <li>
-                <a href="#introduction">Who I am</a>    
-            </li>
-            <li>
-                <a href="#drive-for-japan">The Drive for Japan</a>
-            </li>
-            <li>
-                <a href="#training-arc">The Training Arc</a>
-            </li>
-            <li>
-                <a href="#the-goal">Introduction to HENNGE</a>
-            </li>
-            <li>
-                <a href="#the-journey">HENNGE Journey</a>
-            </li>
-            <li>
-                <a href="#afterwords">Afterwords</a>
-            </li>
-        </ul>
-    </div>
+  </nav>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
