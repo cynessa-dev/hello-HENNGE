866924b98d16546fe975a31563f6769dc0957375
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-09-01 10:33:33 +0800

    🍱 content: add images to training arc section

--- src/components/sections/TrainingArc.vue
+++ src/components/sections/TrainingArc.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+    import teamBlockchain from '@/assets/images/training_0.jpeg';
+    import pictureWithHost from '@/assets/images/training_1.jpeg';
+    import figmaCompetition from '@/assets/images/training_2.jpg';
 </script>
 
 <template>
@@ -10,9 +13,14 @@
         <p>
             During my first year in the university, my seniors greeted me and showed me more cool technologies! They even managed to get me into their community called "Code Wizards", where I became the Vice President in later years. They were the ones who kept pushing me to go beyond my limits, and well, they succeeded! 😅
         </p>
+        <img :src="teamBlockchain" alt="Team Blockchain" />
+        <img :src="pictureWithHost" alt="Picture with Host" />
+        <p>First Blockchain Workshop</p>
         <p>
             Because of that, I kept joining competitions whenever I can to keep challenging myself, even though most of the time I lose, I still kept on competing. Why? Because I get to learn from the best! I get to talk to other contestants and judges, and know their thought process.
         </p>
+        <img :src="figmaCompetition" alt="Figma Competition" />
+        <p>Fruition of my Failures</p>
         <p>
             Though, despite all the learning, there was still one problem. I got no direction! Well, I got a goal, but not how to reach that goal. I wanted to have a path to follow. Questions kept popping, what stack should I master? What kind of developer should I be? What company should I work with? 
         </p>
