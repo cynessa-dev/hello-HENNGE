fb7d79cc7cd8a09e91c889369fbfb85c96b52df4
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-09-01 17:36:32 +0800

    💄 style(Navigation): add background color to navigation bar for better visibility

--- src/components/ui/Navigation.vue
+++ src/components/ui/Navigation.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <nav class="sticky top-0">
+  <nav class="sticky top-0 bg-background">
     <div class="flex justify-between items-center px-4 py-6">
       <h1 class="font-bold">Hello, HENNGE!</h1>
       <ul class="flex space-x-8">
