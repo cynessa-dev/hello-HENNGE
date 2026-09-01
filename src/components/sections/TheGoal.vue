50ab7d403bf5afe428c5f2b36e585f03fce98954
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-09-01 10:50:46 +0800

    🍱 content: add images to the goal section

--- src/components/sections/TheGoal.vue
+++ src/components/sections/TheGoal.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
+    import codeWizards from '@/assets/images/goal_0.jpeg'
+    import hackathon from '@/assets/images/goal_1.jpg'
 </script>
 
 <template>
     <section id="the-goal">
+        <img :src="hackathon" alt="Hackathon" />
+        <p>Joined a Hackathon with a Team of 4</p>
         <h2>Introduction to HENNGE</h2>
         <p>
             This is where it got interesting. I wanted to be structured. Have a guide to follow. So I thought, since I want to work in Japan, I should check industry standards in there. From there I listed a few Japanese companies that I found interesting. And out of those companies, HENNGE was one of best match for me. They are curious, learn-aholic, and continuously growing.
@@ -10,6 +14,8 @@
         <p>
             HENNGE's philosophy, "Liberation of Technolgy", is such an interesting mindset! I watched a video of HENNGE's CEO, Mr. Ogura, explaining that there will always be a gap between technologies and customers because technology keeps evolving, and that they wanted to keep filling the gap. After that, I just knew I gotta join them.
         </p>
+        <img :src="codeWizards" alt="Code Wizards" />
+        <p>Discussing adaption of emerging tech with Code Wizards</p>
         <p>
             With having the HENNGE path, I wanted to adapt their philosophy. Just like how my seniors helped me to grow. I wanted to do the same with everyone. To build solutions to keep filling the gap!
         </p>
