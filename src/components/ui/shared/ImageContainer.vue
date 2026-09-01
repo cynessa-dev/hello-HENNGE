0fb92639e0cde34563382bd412fd58e04a0b9ac3
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-09-01 16:51:30 +0800

    ✨ feat(shared): add ImageContainer for displaying images

---
+++ src/components/ui/shared/ImageContainer.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  interface Image {
+    src: string
+    alt?: string
+  }
+
+  defineProps<{
+    images: Image[]
+    caption: string
+  }>()
+</script>
+
+<template>
+  <div>
+    <div v-for="image in images" :key="image.src">
+      <img :src="image.src" :alt="image.alt" />
+    </div>
+    <p>{{ caption }}</p>
+  </div>
+</template>
+
+<style scoped></style>
