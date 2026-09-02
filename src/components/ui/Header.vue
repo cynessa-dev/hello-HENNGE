23832db4839ed610f58d664569155c278a98c44b
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-09-02 14:46:43 +0800

    💄 style(Header.vue): add responsiveness to text from `sm:` to `md:`

--- src/components/ui/Header.vue
+++ src/components/ui/Header.vue
@@ -2,8 +2,10 @@
 
 <template>
   <header class="mt-12 mb-8">
-    <h1 class="text-4xl font-bold">The Loop: A Journey of Continuous Learning</h1>
-    <div class="flex space-x-2 h-10 text-muted text-sm">
+    <h1 class="text-2xl sm:text-3xl md:text-4xl font-bold">
+      The Loop: A Journey of Continuous Learning
+    </h1>
+    <div class="flex space-x-2 h-10 text-muted text-xs md:text-sm">
       <p>Christian Mamplata</p>
       <p>·</p>
       <p>August 31, 2026</p>
