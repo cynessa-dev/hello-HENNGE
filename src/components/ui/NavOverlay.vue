a1ef36f91b245afdcebff75dd9350db221fe31cf
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-09-03 08:51:20 +0800

    ✨ feat(NavOverlay.vue): add NavOverlay component for mobile navigation

---
+++ src/components/ui/NavOverlay.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+  interface MenuItem {
+    section: string
+    link: string
+  }
+
+  defineProps<{
+    menuItems: MenuItem[]
+  }>()
+</script>
+
+<template>
+  <div class="overlay">
+    <ul class="flex flex-col px-4 py-6 text-base font-bold transition-colors lg:hidden">
+      <li v-for="item in menuItems">
+        <a :href="item.link">{{ item.section }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+  li {
+    padding-block: 1rem;
+    width: 100%;
+    text-align: right;
+    border-bottom: 1px solid var(--color-border);
+  }
+
+  li:first-child {
+    border-top: 1px solid var(--color-border);
+  }
+
+  .overlay {
+    position: fixed;
+    opacity: 0;
+    margin-top: -1px;
+    width: 100%;
+    background-color: var(--color-background);
+    transition: opacity 200ms linear;
+  }
+
+  .overlay.active {
+    opacity: 1;
+  }
+</style>
