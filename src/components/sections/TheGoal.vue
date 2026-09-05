0ca60a757aadd8289bf0e06bc8ddb22c08c3be6e
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-09-05 14:27:56 +0800

    ✏️ content(TheGoal): update to be semi-formal

--- src/components/sections/TheGoal.vue
+++ src/components/sections/TheGoal.vue
@@ -30,14 +30,15 @@
     <p>
       This is where it got interesting. If I wanted to be the kind of developer to try out new
       things, and use that knowledge to help others, I needed to go out. So I started to search for
-      company that has the exact environment I wanted to be in. And I wanted to be in Japan, I
-      listed a few Japanese companies that I found interesting. And out of those companies, HENNGE
-      was one of best match for me. They are curious, learn-aholic, and continuously growing. Their
-      ability to adapt to the continuous evolution to technology always amazes me.
+      company that has the exact environment I wanted to be in. My dream was to get a career in
+      Japan, so I searched a few Japanese companies that were offering internships. Scrolling
+      through TokyoDev, I came across HENNGE, and the environment they were developing was a perfect
+      match for me. They are curious, learnaholic, and continuously growing. Their ability to adapt
+      to the continuous evolution to technology always amazes me.
     </p>
     <p>
       HENNGE's philosophy, "Liberation of Technolgy", is such an interesting mindset! I watched a
-      video of HENNGE's CEO, Mr. Ogura, explaining that there will always be a gap between
+      video of HENNGE's CEO, Ogura-san, explaining that there will always be a gap between
       technologies and customers because technology keeps evolving, and that they wanted to keep
       filling the gap. After that, I just knew I gotta join them.
     </p>
@@ -45,8 +46,8 @@
     <ImageContainer :images="hackathonImage" caption="Joined a Hackathon with a Team of 4" />
 
     <p>
-      With having the HENNGE path, I wanted to adapt their philosophy. Just like how my seniors
-      helped me to grow. I wanted to do the same with everyone. To build solutions to keep filling
+      With getting to know HENNGE, I wanted to adapt their philosophy. Just like how my seniors
+      helped me to grow, I wanted to do the same for everyone. To build solutions to keep filling
       the gap!
     </p>
   </section>
