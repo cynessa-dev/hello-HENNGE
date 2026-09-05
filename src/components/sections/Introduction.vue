31ad6b045d375196d0e885b95dd74633dd158621
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-09-05 13:32:39 +0800

    ✏️ content(Introduction): update to be semi-formal

--- src/components/sections/Introduction.vue
+++ src/components/sections/Introduction.vue
@@ -18,9 +18,10 @@
     <h2>Who I am</h2>
     <p>
       Hi, there! Thanks for checking this blog out. I hope this will help you to get to know me
-      better, and have some laughs 😆. Do note that this will be a casual blog, as I believe that I
-      can express myself better this way without thinking about formal words, just focusing on
-      storytelling.
+      better, and have some laughs 😆. This was made as a stepping stone for learning Vue. Recently,
+      I have been itching to try it out and see how I will be able to use it in future problems,
+      along with learning some new tools to help me out. Additionally, the blog is made to document
+      my progress since my first attempt to join HENNGE (Maybe they'll see this too).
     </p>
     <p>
       A little about myself: I am a graduating student of Information Technology from a public
@@ -28,6 +29,7 @@
       Technology. Quite a long name, ain't it? Before I met HENNGE, I was already in love with
       Japan. The culture, the environment, the opportunities. It was just perfect!
     </p>
+    <p>Well, that's all for introductions, hope you enjoy your read!</p>
   </section>
 </template>
 
